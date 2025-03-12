a886d0051648d77a0ebc2f80244de06afeb9ac24
samettok0 <99971700+samettok0@users.noreply.github.com>
2025-03-12 21:17:14 +0300

    Face recognition tested with unknown.jpg
    I tested the detector with a picture of me and my brother and a picture of me and my friend. In both pictures result was accurate.

--- detector.py
+++ detector.py
@@ -1,5 +1,6 @@
 import pickle
 from pathlib import Path
+from collections import Counter
 
 import face_recognition
 
@@ -35,5 +36,47 @@ def encode_known_faces(
     with encodings_location.open(mode="wb") as f:
         pickle.dump(name_encodings, f)
 
+# encode_known_faces()
 
-encode_known_faces()
+def recognize_faces(
+    image_location: str,
+    model: str = "hog",
+    encodings_location: Path = DEFAULT_ENCODINGS_PATH,
+) -> None:
+    with encodings_location.open(mode="rb") as f:
+        loaded_encodings = pickle.load(f)
+
+    input_image = face_recognition.load_image_file(image_location)
+
+    input_face_locations = face_recognition.face_locations(
+        input_image, model=model
+    )
+    input_face_encodings = face_recognition.face_encodings(
+        input_image, input_face_locations
+    )
+
+
+    for bounding_box, unknown_encoding in zip(
+        input_face_locations, input_face_encodings
+    ):
+        name = _recognize_face(unknown_encoding, loaded_encodings)
+        if not name:
+            name = "Unknown"
+        print(name, bounding_box)
+
+def _recognize_face(unknown_encoding, loaded_encodings):
+    boolean_matches = face_recognition.compare_faces(
+        loaded_encodings["encodings"], unknown_encoding
+    )
+    votes = Counter(
+        name
+        for match, name in zip(boolean_matches, loaded_encodings["names"])
+        if match
+    )
+    if votes:
+        return votes.most_common(1)[0][0]
+
+recognize_faces("unknown.jpg")
+
+
+
